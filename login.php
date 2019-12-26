db7a06349a4d6577f1b8c38e023a2be9631a979a
TeemuKorho <vanhaukki93@gmail.com>
2019-12-26 15:19:35 +0200

    Pääsivun ja kirjautumisen tekoa

--- login.php
+++ login.php
@@ -1,89 +1,84 @@
 <?php
-// Initialize the session
 session_start();
  
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+{
   header("location: paasivu.html");
   exit;
 }
  
-// Include config file
 require_once "config.php";
  
-// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
  
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
  
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if(empty(trim($_POST["username"])))
+	{
         $username_err = "Please enter username.";
-    } else{
+    } else
+	{
         $username = trim($_POST["username"]);
     }
     
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if(empty(trim($_POST["password"])))
+	{
         $password_err = "Please enter your password.";
-    } else{
+    } 
+	else
+	{
         $password = trim($_POST["password"]);
     }
     
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
+    if(empty($username_err) && empty($password_err))
+	{
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
+        if($stmt = mysqli_prepare($link, $sql))
+		{
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
             $param_username = $username;
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
+            if(mysqli_stmt_execute($stmt))
+			{
                 mysqli_stmt_store_result($stmt);
                 
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
+                if(mysqli_stmt_num_rows($stmt) == 1)
+				{
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
+                    if(mysqli_stmt_fetch($stmt))
+					{
+                        if(password_verify($password, $hashed_password))
+						{
                             session_start();
                             
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;                            
                             
-                            // Redirect user to welcome page
                             header("location: paasivu.html");
-                        } else{
-                            // Display an error message if password is not valid
+                        } 
+						else
+						{
                             $password_err = "The password you entered was not valid.";
                         }
                     }
-                } else{
-                    // Display an error message if username doesn't exist
+                } else
+				{
                     $username_err = "No account found with that username.";
                 }
-            } else{
+            } else
+			{
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
         
-        // Close statement
         mysqli_stmt_close($stmt);
     }
     
-    // Close connection
     mysqli_close($link);
 }
 ?>
