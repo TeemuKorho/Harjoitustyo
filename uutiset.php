6a085cf20ea80c14705d30077674f444e4ed5639
TeemuKorho <vanhaukki93@gmail.com>
2019-12-28 14:52:30 +0200

    Pääsivun tekoa ja uutisia :D

--- uutiset.php
+++ uutiset.php
@@ -1,16 +1,21 @@
 <?php
 echo "<h2>Presidentti Trump valittiin kolmannelle virkakaudelle</h2>";
-echo "<p>Murskavoitto repuplikaaneille</p>";
+echo "<p>Murskavoitto repuplikaaneille.</p>";
 echo "<br>";
 echo "<br>";
 
 echo "<h2>Demokraattien entinen presidenttiehdokas Hillary Clinton vankilaan lukuisten rikosten vuoksi</h2>";
-echo '<p>Bill Clinton: "En voinut pitää Hillaryn rikoksia salassa enää."</p>';
+echo '<p>Bill Clinton: "En voinut pitää hänen rikoksiaan salassa enää. Sain tarpeekseni. Presidentti Trump oli oikeassa alusta asti."</p>';
 echo "<br>";
 echo "<br>";
 
-echo "<h2>Demokraattien puolue hajosi</h2>";
-echo "<p>Saatuaan murskavoiton vuoden 2020 presidentinvaaleissa Presidentti Donald Trump ei ainoastaan murskannut 
-äänestysprosentin, mutta myös koko demokraatti puolueen.</p>";
-echo "<p>Hävittyään 2020 vaalit, demokraatit näyttivät todellisen nahkansa ja menettivät 95% äänestäjistään.</p>";
+echo '<h2>"Uutiskanavat" CNN ja MSNBC suljettiin</h2>';
+echo "<p>Levitettyään valeuutisia jo vuosikausien ajan, CNN ja MSNBC joutuivat vihdoinkin sulkemaan ovensa saatuaan tuhansia 
+oikeushaasteita.</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Twitter haastettu oikeuteen sensuroinnista</h2>";
+echo "<p>Suosittu mikroblogisivusto Twitter haastettiin oikeuteen sen jälkeen kun tuli ilmi, että Twitterin omistajat sensuroivat 
+konservatiivisia käyttäjiä ja antoivat heille kieltoja ilman mitään syitä.</p>";
 ?>
