4f3fce9a8ce7bed684035a66f47e989c29ec5996
TeemuKorho <vanhaukki93@gmail.com>
2019-12-29 13:54:58 +0200

    Pieniä muutoksia

--- uutiset.php
+++ uutiset.php
@@ -1,21 +1,51 @@
 <?php
-echo "<h2>Presidentti Trump valittiin kolmannelle virkakaudelle</h2>";
-echo "<p>Murskavoitto repuplikaaneille.</p>";
+echo "<h2>Uutisotsikko 1</h2>";
+echo "<p>Kuvaus uutiselle 1</p>";
 echo "<br>";
 echo "<br>";
 
-echo "<h2>Demokraattien entinen presidenttiehdokas Hillary Clinton vankilaan lukuisten rikosten vuoksi</h2>";
-echo '<p>Bill Clinton: "En voinut pitää hänen rikoksiaan salassa enää. Sain tarpeekseni. Presidentti Trump oli oikeassa alusta asti."</p>';
+echo "<h2>Uutisotsikko 2</h2>";
+echo "<p>Kuvaus uutiselle 2</p>";
 echo "<br>";
 echo "<br>";
 
-echo '<h2>"Uutiskanavat" CNN ja MSNBC suljettiin</h2>';
-echo "<p>Levitettyään valeuutisia jo vuosikausien ajan, CNN ja MSNBC joutuivat vihdoinkin sulkemaan ovensa saatuaan tuhansia 
-oikeushaasteita.</p>";
+echo "<h2>Uutisotsikko 3</h2>";
+echo "<p>Kuvaus uutiselle 3</p>";
 echo "<br>";
 echo "<br>";
 
-echo "<h2>Twitter haastettu oikeuteen sensuroinnista</h2>";
-echo "<p>Suosittu mikroblogisivusto Twitter haastettiin oikeuteen sen jälkeen kun tuli ilmi, että Twitterin omistajat sensuroivat 
-konservatiivisia käyttäjiä ja antoivat heille kieltoja ilman mitään syitä.</p>";
+echo "<h2>Uutisotsikko 4</h2>";
+echo "<p>Kuvaus uutiselle 4</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 5</h2>";
+echo "<p>Kuvaus uutiselle 5</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 6</h2>";
+echo "<p>Kuvaus uutiselle 6</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 7</h2>";
+echo "<p>Kuvaus uutiselle 7</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 8</h2>";
+echo "<p>Kuvaus uutiselle 8</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 9</h2>";
+echo "<p>Kuvaus uutiselle 9</p>";
+echo "<br>";
+echo "<br>";
+
+echo "<h2>Uutisotsikko 10</h2>";
+echo "<p>Kuvaus uutiselle 10</p>";
+echo "<br>";
+echo "<br>";
 ?>
